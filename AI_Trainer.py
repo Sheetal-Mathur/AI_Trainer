c3fb8d6d8ccb565b67be0adacd3ec9915ed183ea
Sheetal-Mathur <mathursheetal94@gmail.com>
2022-04-13 15:17:59 +0530

    template

--- AI_Trainer.py
+++ AI_Trainer.py
@@ -2,9 +2,9 @@ import numpy as np
 import time
 import cv2
 import posemodule as pm
-import rf_random
 
-cap=cv2.VideoCapture("C://Users//mathu//Downloads//curl.mp4")
+
+cap=cv2.VideoCapture("C://Users//mathu//Downloads//p2.mp4")
 
 detector=pm.poseDetector()
 pTime=0
@@ -12,17 +12,16 @@ count = 0
 dir = 0
 while True:
     success, img=cap.read()
-    #img = cv2.imread("C://Users//mathu//Downloads//pexels-gustavo-fring-3984372.jpg")
-    img=cv2.resize(img,(1280,600))
+    #img=cv2.resize(img,(600,400))
     img=detector.findPose(img,False) #for draw line
     lmList=detector.findPosition(img,False) #for creating dots
     #print(lmList)
     if len(lmList)!=0:
         #for left arm
         angle=detector.findAngle(img,11,13,15)
-        angle = detector.findAngle(img, 12, 14, 16)
-        per = np.interp(angle, (210,310), (0, 100))
-        bar = np.interp(angle, (220, 310), (650, 100))
+        #angle = detector.findAngle(img, 12, 14, 16)
+        per = np.interp(angle, (210,310), (0, 100)) #2nd shows the  to 100%
+        bar = np.interp(angle, (220, 310), (400, 100))  #2nd shows bar height n width
 
 
         #per = np.interp(angle, (210, 310), (0, 100))
@@ -45,14 +44,14 @@ while True:
                 dir = 0
         print(count)
         # Draw Bar
-        cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
-        cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
-        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
+        cv2.rectangle(img, (250, 100), (200, 400), color, 2)
+        cv2.rectangle(img, (250, int(bar)), (200, 400), color, cv2.FILLED)
+        cv2.putText(img, f'{int(per)} %', (210, 65), cv2.FONT_HERSHEY_PLAIN, 4,
                     color, 4)
         #DRAW CURLS
-        cv2.rectangle(img, (0, 450), (200, 700), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(int(count)), (20, 600), cv2.FONT_HERSHEY_PLAIN, 10,
-                    (255.0,0), 10)
+        cv2.rectangle(img, (0, 550), (100, 700), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (20, 620), cv2.FONT_HERSHEY_PLAIN, 5,
+                    (255.0,0), 5)
     '''cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
@@ -64,9 +63,11 @@ while True:
     key=cv2.waitKey(1)
     if key == ord("q") or key == ord("Q"):
         break
+
 cap.release()
 cv2.destroyAllWindows()
 
 import pickle
-file=open("AI_Trainer.pkl","wb")
-pickle.dump(,file)
+file=open("AI_TRAINER.pkl","wb")
+pickle.dump(lmList,file)
+file.close()
